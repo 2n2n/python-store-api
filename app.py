d3492535ee272e1e95cdd74a027c0930240143c9
2n2n <ayolachll@gmail.com>
2017-03-31 07:30:07 +0800

    commit modified run.py

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = '2n2n'
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-	db.create_all()
 	
 jwt = JWT(app, authenticate, identity)
 
